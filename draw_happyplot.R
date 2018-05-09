dc63cdc7c7ce37e3649fffea4b22dc0230f7f841
Tom Vaughan <31933859+tlvaughan@users.noreply.github.com>
2018-05-09 16:09:47 -0700

    happyplot corrections 9may18

--- draw_happyplot.R
+++ draw_happyplot.R
@@ -7,17 +7,24 @@ y=seq(1,40, length= 40)
 
 happyfield= merge(x, y, by= NULL)
 happyfield= dplyr::rename(happyfield, xcol=x, ycol=y)
-happyfield= arrange(happyfield, desc(ycol), xcol)
+happyfield= arrange(happyfield, desc(ycol), xcol) #descending order for rows
 
-happyfield= mutate(happyfield, newrow=seq(1:1000))
-happyfield= mutate(happyfield, case= ifelse(newrow<=(numcases),0,1))
-happyfield= mutate(happyfield, case_shape= ifelse(case==1,21,19))
+happyfield= mutate(happyfield, newrow=seq(1:1000))  #begins at x,y= 1,40
+happyfield= mutate(happyfield, case= ifelse(newrow>(numcases), 1, 0))  #cases are coded 0; non-cases= 1
+happyfield= mutate(happyfield, case_shape= ifelse(case==1, 21, 19))
 
-mycolors= c("#cc0000", "black")
+# View(happyfield)
 
+if(numcases>0) {
+  mycolors= c("#cc0000", "black")
+  } else {
+    mycolors= c("black", "black")
+  }
+# mycolors= c("#cc0000", "black")
+# View((mycolors))
 p= ggplot(happyfield, aes(xcol, ycol))  +
   geom_point(data= happyfield, mapping= aes(x= xcol, y= ycol, 
-                                            color= factor(case)), 
+                                            color= factor(case)),
              size=2.8, shape= happyfield$case_shape)  +
   scale_shape_discrete() +
   scale_colour_manual(values = mycolors) +
--- required_files/bg_help.md
+++ required_files/bg_help.md
@@ -1,1 +1,1 @@
-* EA is quite rare in the female/black category, so no reliable data are available.
+* EA is quite rare in the female/black category, so no reliable data are available (see "About" tab)
